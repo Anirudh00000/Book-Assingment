ddc3124101d185bfc391b01b33a31a678d9a4186
ANIRUDH SINGH <anirudh8400@gmail.com>
2022-08-10 09:54:43 +0530

    first commit

---
+++ New folder/BooksellingAPi/BookDbContext.cs
@@ -0,0 +1,28 @@
+﻿using BooksellingAPi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksellingAPi
+{
+    public class BookDbContext:DbContext
+    {
+
+        public DbSet<Book> BookDetails { get; set; }
+
+        public BookDbContext()
+        {
+
+        }
+
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-0GGBKPE\\SQLEXPRESS;Initial Catalog=SellBook;Integrated Security=True");
+        }
+
+    }
+
+}
+
